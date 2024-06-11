7f8d1ea20ec14a4e01c49cbe929107df4ab3cad7
muryloandrade <muryloandrades@gmail.com>
2024-06-11 15:23:08 -0300

    Polygon

--- webrota-front/src/app/components/map/map.component.ts
+++ webrota-front/src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { LatLngExpression } from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -15,7 +16,25 @@ export class MapComponent implements OnInit {
     this.map = L.map('map').setView([46.879966, -121.726909], 7);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
+
+    const polygonPoints = [
+      [46.879966, -121.726909], // Ponto 1
+      [46.879966, -121.726909 + 0.1], // Ponto 2
+      [46.879966 + 0.1, -121.726909 + 0.1], // Ponto 3
+    ];
+
+    L.polygon(
+      polygonPoints as
+        | LatLngExpression[]
+        | LatLngExpression[][]
+        | LatLngExpression[][][],
+      {
+        color: 'blue', // Cor da borda
+        fillColor: 'lightblue', // Cor de preenchimento
+        fillOpacity: 0.5, // Opacidade do preenchimento
+      }
+    ).addTo(this.map);
   }
 }
